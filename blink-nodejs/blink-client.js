fa0b2de04e096ee92a01b3bc4e8f7e08aaf7e709
lyndaweiss <64429889+lyndaweiss@users.noreply.github.com>
2020-11-12 17:15:11 -0800

    Removed unnecessary lines of code

--- blink-nodejs/blink-client.js
+++ blink-nodejs/blink-client.js
@@ -1,7 +1,6 @@
 {
   let socket = io('localhost:8000');
-  // const socket = io('http://192.168.0.49:8000');
-
+  
   const ledBlinkButton = document.querySelector('#ledBlink');
   const numBlinksInput = document.querySelector('#ledNumBlink');
   const delayBlinksInput = document.querySelector('#ledBlinkDelay');
@@ -10,7 +9,7 @@
         blinks: numBlinksInput.value,
         delay: delayBlinksInput.value,
       };
-      console.log(`Blinking LED ${blinkData['blinks']} times.`);
+
       socket.emit('led', blinkData);
   });
 }
